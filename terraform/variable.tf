b63af6e3849ee4085918baafd53491b6aa85620c
tanlye <tan_lovell@hotmail.com>
2024-11-06 20:52:46 +0800

    v0.1.0

---
+++ terraform/variable.tf
@@ -0,0 +1,17 @@
+
+variable "lambda_function_name" {
+  description = "Name of lambda function"
+  type        = string
+  default     = "lovell-lambda"
+}
+
+variable "lambda_file_name" {
+  description = "Name of lambda file to be zipped"
+  type        = string
+  default     = "lovell-lambda"
+}
+
+variable "REGION" {
+  type        = string
+  default     = "us-east-1"
+}
